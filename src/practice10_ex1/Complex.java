2b524c3c8bbca847456ecba4a691d3238a594b43
rel1se <botoevslava27032003@mail.ru>
2022-10-07 02:15:14 +0300

    добавлена 10 практика 1 упражнение

---
+++ src/practice10_ex1/Complex.java
@@ -0,0 +1,9 @@
+package practice10_ex1;
+
+public class Complex {
+    int real,image;
+    Complex(int real, int image) {
+        this.real = real;
+        this.image = image;
+    }
+}
---
+++ src/practice10_ex1/ComplexAbstractFactory.java
@@ -0,0 +1,6 @@
+package practice10_ex1;
+
+public interface ComplexAbstractFactory {
+    Complex createComplex();
+    Complex CreateComplex(int real,int image);
+}
